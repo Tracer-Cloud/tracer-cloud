37539963d4232acfaf9ce5628c761225e062e6c2
David Onuh <davidonuh1@gmail.com>
2025-01-24 20:11:36 +0100

    otel complains use sysmetrics

--- examples/otel_compliance.rs
+++ examples/otel_compliance.rs
@@ -1,14 +1,10 @@
 use reqwest::Client;
 use serde_json::json;
 use serde_json::Value;
-use tracer::config_manager::ConfigManager;
-use tracer::exporters::FsExportHandler;
-use tracer::exporters::S3ExportHandler;
-use tracer::monitor_processes_with_tracer_client;
-use tracer::tracer_client::TracerClient;
+use tracer::event_recorder::EventRecorder;
+use tracer::metrics::SystemMetricsCollector;
 use tracer::tracing::init_tracing;
 
-use tracer::types::config::AwsConfig;
 use tracer::types::event::Event;
 
 use sysinfo::System;
@@ -19,45 +15,25 @@ async fn main() {
 
     tokio::spawn(loki_task);
 
+    let collector = SystemMetricsCollector::new();
     let run_name = format!("local_otel_compliance");
+    let mut recorder = EventRecorder::new(Some(run_name.clone()), Some(run_name));
     let mut system = System::new();
-    let raw_config = ConfigManager::load_config();
 
-    let export_dir =
-        ConfigManager::get_tracer_parquet_export_dir().expect("Failed to get export dir");
-
-    let fs_handler = FsExportHandler::new(export_dir, None);
-    let s3_handler = S3ExportHandler::new(
-        fs_handler,
-        //AwsConfig::Profile("me".to_string()),
-        raw_config.aws_init_type.clone(),
-        raw_config.aws_region.as_str(),
-    )
-    .await;
-
-    let current_working_directory =
-        std::env::current_dir().expect("Failed to get current working dir");
-
-    let mut client = TracerClient::new(
-        raw_config.clone(),
-        current_working_directory.to_str().unwrap().to_string(),
-        s3_handler,
-    )
-    .await
-    .expect("failed to create client");
+    println!("beginning");
 
     let mut count = 5;
 
     while count > 0 {
-        monitor_processes_with_tracer_client(&mut client)
-            .await
-            .expect("Failed to monitor_processes_with_tracer_client");
+        let _ = collector.collect_metrics(&mut system, &mut recorder);
         count -= 1;
         std::thread::sleep(std::time::Duration::from_millis(100));
-        // system.refresh_all();
+        system.refresh_all();
     }
 
-    let data = client.logs.get_events();
+    let data = recorder.get_events();
+
+    println!("data from events: {:?}", data);
 
     for event in data.iter() {
         push_to_loki(event).await.expect("Failed to push to loki")
