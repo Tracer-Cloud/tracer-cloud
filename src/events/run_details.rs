e581c90327d1e911a02672b8961d2044db4154d0
David Onuh <davidonuh1@gmail.com>
2025-02-03 14:03:53 +0100

    remove generated pipeline name func

--- src/events/run_details.rs
+++ src/events/run_details.rs
@@ -1,7 +1,6 @@
 use once_cell::sync::Lazy;
 use rand::seq::SliceRandom;
 use rand::Rng;
-use sha2::{Digest, Sha256};
 
 static ADJECTIVES: Lazy<Vec<&str>> =
     Lazy::new(|| vec!["snowy", "silent", "desert", "mystic", "ancient"]);
@@ -24,41 +23,4 @@ pub(super) fn generate_run_name() -> String {
 
 pub(super) fn generate_run_id() -> String {
     uuid::Uuid::new_v4().to_string()
-}
-
-#[allow(dead_code)]
-pub(super) fn generate_pipeline_name_from_key(api_key: &str) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(api_key);
-    let hash = hasher.finalize();
-
-    // Convert hash to a deterministic sequence
-    let hash_bytes = hash.as_slice();
-
-    // Use the hash to pick an adjective and an animal
-    let animal_index = (hash_bytes[1] as usize) % ANIMALS.len();
-
-    let animal = ANIMALS[animal_index];
-
-    // Use part of the hash for a unique fragment
-    let key_fragment = format!("{}", hash_bytes[hash_bytes.len() - 1]);
-
-    format!("pipeline-{}-{}", animal, key_fragment)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::generate_pipeline_name_from_key;
-
-    #[test]
-    fn test_pipeline_key_generates_same_name_per_key() {
-        let api_key = "z12s33";
-        let mut count = 0;
-        let output = "pipeline-bear-247";
-
-        while count < 5 {
-            assert_eq!(output, generate_pipeline_name_from_key(api_key));
-            count += 1
-        }
-    }
 }
