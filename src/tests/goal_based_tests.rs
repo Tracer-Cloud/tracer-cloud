e44d0a0b425946e899f34d243c3d8d78f403aaed
David Onuh <davidonuh1@gmail.com>
2025-02-06 15:03:09 +0100

    make query generic over serde deserialize

--- src/tests/goal_based_tests.rs
+++ src/tests/goal_based_tests.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use std::time::Duration;
 use std::time::Instant;
 
+use super::query;
 use crate::config_manager::target_process::target_matching::TargetMatch;
 use crate::config_manager::target_process::DisplayName;
 use crate::config_manager::target_process::Target;
@@ -11,9 +12,7 @@ use crate::exporters::ParquetExport;
 use crate::extracts::file_watcher::FileWatcher;
 use crate::extracts::process_watcher::ProcessWatcher;
 use crate::types::parquet::FlattenedTracerEvent;
-use arrow::array::RecordBatch;
-use arrow::json::ArrayWriter;
-use duckdb::Connection;
+use itertools::Itertools;
 use sysinfo::System;
 use tempdir::TempDir;
 
@@ -23,36 +22,6 @@ fn setup_fs_exporter_and_path() -> (FsExportHandler, PathBuf) {
     let export_dir = temp_dir.path().to_path_buf();
 
     (FsExportHandler::new(export_dir.clone(), None), export_dir)
-}
-
-async fn query(query: &str) -> Vec<FlattenedTracerEvent> {
-    let conn = Connection::open_in_memory().expect("Failed to create duckdb connection");
-    let mut stmt = conn.prepare(query).expect("Query failed");
-
-    let records: Vec<RecordBatch> = stmt
-        .query_arrow([])
-        .expect("failed to convert to record batch")
-        .collect();
-
-    records
-        .iter()
-        .map(record_batch_to_structs)
-        .flatten()
-        .collect()
-}
-
-fn record_batch_to_structs(batch: &RecordBatch) -> Vec<FlattenedTracerEvent> {
-    // Convert RecordBatch to JSON
-    let buf = Vec::new();
-    let mut writer = ArrayWriter::new(buf);
-    writer.write_batches(&[batch]).unwrap();
-    writer.finish().unwrap();
-
-    let buf = writer.into_inner();
-    let json_str = String::from_utf8(buf).unwrap();
-
-    // Deserialize JSON into structs
-    serde_json::from_str::<Vec<FlattenedTracerEvent>>(&json_str).unwrap()
 }
 
 async fn run_process_watcher(
@@ -84,9 +53,8 @@ async fn run_process_watcher(
     }
 }
 
-#[ignore = "Not complete"]
 #[tokio::test]
-async fn test_tools_tracked_based_on_targets() {
+async fn test_query_via_duckdb_works() {
     let total_duration = 10; // Total monitoring duration in seconds
     let python_ration = 0.6; // 60% of the time for python and 40% for top
     let file_path = "test-files/scripts/monitor.sh";
@@ -130,8 +98,86 @@ async fn test_tools_tracked_based_on_targets() {
         export_dir.as_path().to_str().unwrap()
     );
 
-    let _ = query(&query_str).await;
+    let query_res: Vec<FlattenedTracerEvent> = query(&query_str).await;
+
+    assert!(!query_res.is_empty());
+
+    // cleanup
+    let _ = std::fs::remove_dir_all(export_dir);
+    let _ = output.kill();
+}
+
+#[tokio::test]
+async fn test_tools_tracked_based_on_targets() {
+    let total_duration = 10; // Total monitoring duration in seconds
+    let python_ration = 0.6; // 60% of the time for python and 40% for top
+    let file_path = "test-files/scripts/monitor.sh";
+
+    let targets = vec![
+        Target::new(TargetMatch::ProcessName("python".to_string()))
+            .set_display_name(DisplayName::Default()),
+        Target::new(TargetMatch::ProcessName("python2".to_string()))
+            .set_display_name(DisplayName::Default()),
+        Target::new(TargetMatch::ProcessName("python3".to_string()))
+            .set_display_name(DisplayName::Default()),
+        Target::new(TargetMatch::ProcessName("top".to_string()))
+            .set_display_name(DisplayName::Default()),
+    ];
+
+    // execute scripts
+    let mut output = std::process::Command::new(file_path)
+        .arg(total_duration.to_string())
+        .arg(python_ration.to_string())
+        .spawn()
+        .expect("failed to run script");
+
+    let mut events_recorder = EventRecorder::default();
+    run_process_watcher(
+        &mut events_recorder,
+        Duration::from_secs(total_duration),
+        targets,
+    )
+    .await;
+
+    let data = events_recorder.get_events();
+
+    let (handler, export_dir) = setup_fs_exporter_and_path();
+    handler
+        .output(data, "testing")
+        .await
+        .expect("failed to output");
+
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    struct ProcessSubSet {
+        tool_pid: Option<String>,
+        tool_name: String,
+    }
+
+    let mut expected_tool_names = vec!["python3".to_string(), "top".to_string()];
+    expected_tool_names.sort();
+
+    let query_processes_for_a_run_name = format!(
+        r#"select process_attributes.tool_pid, process_attributes.tool_name
+        from "{}/**/*.parquet"
+        where process_attributes.tool_pid is not Null
+        group by 
+        process_attributes.tool_name, process_attributes.tool_pid;"#,
+        export_dir.as_path().to_str().unwrap()
+    );
+
+    let query_res: Vec<ProcessSubSet> = query(&query_processes_for_a_run_name).await;
+
+    assert_eq!(query_res.len(), 2);
+
+    let queried_process_names: Vec<String> = query_res
+        .into_iter()
+        .map(|p| p.tool_name)
+        .sorted()
+        .collect();
+
+    assert_eq!(queried_process_names, expected_tool_names);
 
     // cleanup
     let _ = output.kill();
+    let _ = std::fs::remove_dir_all(export_dir);
 }
