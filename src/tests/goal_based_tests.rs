960194661b4d353d496e6642209b1c555b074a2c
David Onuh <davidonuh1@gmail.com>
2025-02-06 02:21:14 +0100

    WIP: implement process watcher based on little script. Next steps would be to query parquet files assert results

---
+++ src/tests/goal_based_tests.rs
@@ -0,0 +1,95 @@
+use std::path::PathBuf;
+use std::time::Duration;
+use std::time::Instant;
+
+use crate::config_manager::target_process::target_matching::TargetMatch;
+use crate::config_manager::target_process::DisplayName;
+use crate::config_manager::target_process::Target;
+use crate::events::recorder::EventRecorder;
+use crate::exporters::FsExportHandler;
+use crate::exporters::ParquetExport;
+use crate::extracts::file_watcher::FileWatcher;
+use crate::extracts::process_watcher::ProcessWatcher;
+use sysinfo::System;
+use tempdir::TempDir;
+
+fn setup_fs_exporter_and_path() -> (FsExportHandler, PathBuf) {
+    let temp_dir = TempDir::new("exports").expect("failed to create tempdir");
+
+    let export_dir = temp_dir.path().to_path_buf();
+
+    (FsExportHandler::new(export_dir.clone(), None), export_dir)
+}
+
+async fn run_process_watcher(
+    events_recorder: &mut EventRecorder,
+    duration: Duration,
+    targets: Vec<Target>,
+) {
+    let mut process_watcher = ProcessWatcher::new(targets);
+
+    let mut system = System::new();
+
+    let file_watcher = FileWatcher::new();
+
+    let start_time = Instant::now();
+
+    while start_time.elapsed() < duration {
+        process_watcher
+            .poll_processes(&mut system, events_recorder, &file_watcher)
+            .expect("Failed to poll processes");
+        process_watcher
+            .poll_process_metrics(
+                &system,
+                events_recorder,
+                std::time::Duration::from_millis(3),
+            )
+            .expect("failed to poll process metrices");
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        system.refresh_all();
+    }
+}
+
+#[ignore = "Not complete"]
+#[tokio::test]
+async fn test_tools_tracked_based_on_targets() {
+    let total_duration = 10; // Total monitoring duration in seconds
+    let python_ration = 0.6; // 60% of the time for python and 40% for top
+    let file_path = "test-files/scripts/monitor.sh";
+
+    let targets = vec![
+        Target::new(TargetMatch::ProcessName("python".to_string()))
+            .set_display_name(DisplayName::UseFirstArgumentBaseName()),
+        Target::new(TargetMatch::ProcessName("python2".to_string()))
+            .set_display_name(DisplayName::UseFirstArgumentBaseName()),
+        Target::new(TargetMatch::ProcessName("python3".to_string()))
+            .set_display_name(DisplayName::UseFirstArgumentBaseName()),
+        Target::new(TargetMatch::ProcessName("top".to_string()))
+            .set_display_name(DisplayName::UseFirstArgumentBaseName()),
+    ];
+
+    // execute scripts
+    let mut output = std::process::Command::new(file_path)
+        .arg(total_duration.to_string())
+        .arg(python_ration.to_string())
+        .spawn()
+        .expect("failed to run script");
+
+    let mut events_recorder = EventRecorder::default();
+    run_process_watcher(
+        &mut events_recorder,
+        Duration::from_secs(total_duration),
+        targets,
+    )
+    .await;
+
+    let data = events_recorder.get_events();
+
+    let _ = output.kill().expect("failed to join task");
+
+    let (handler, _export_dir) = setup_fs_exporter_and_path();
+    handler
+        .output(data, "testing")
+        .await
+        .expect("failed to output");
+}
---
+++ test-files/scripts/monitor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <total_duration_seconds> <python_time_ratio>"
+    exit 1
+fi
+
+total_duration=$1
+python_ratio=$2
+
+# Calculate Python and top durations
+python_duration=$(echo "$total_duration * $python_ratio" | bc)
+top_duration=$(echo "$total_duration - $python_duration" | bc)
+
+# Run Python script for the calculated duration
+python3 -c 'import time; [print(i) or time.sleep(1) for i in range(1, 1000)]' &
+PYTHON_PID=$!
+
+# Sleep for the Python duration
+sleep "$python_duration"
+
+# Kill the Python process
+kill $PYTHON_PID
+
+# Run top for the remaining duration
+timeout "$top_duration"s top -d
