40a3871ec0437d95201b75e1872df0a8b7079b80
David Onuh <davidonuh1@gmail.com>
2025-02-18 11:05:10 +0100

    remove debug prints

--- src/tracer_client.rs
+++ src/tracer_client.rs
@@ -162,8 +162,6 @@ impl TracerClient {
             "annoymous"
         };
 
-        println!("current_run_name .. {run_name}");
-
         if self.last_sent.is_none() || Instant::now() - self.last_sent.unwrap() >= self.interval {
             self.metrics_collector
                 .collect_metrics(&mut self.system, &mut self.logs)
@@ -473,7 +471,6 @@ mod tests {
 
         let work_dir = temp_dir.path().to_str().unwrap();
 
-        println!("db url .... {}", &config.db_url);
         // Create an instance of AuroraClient
         let db_client = Arc::new(AuroraClient::new(&config.db_url, Some(1)).await);
         let pipeline_name = String::from("test_pipeline");
