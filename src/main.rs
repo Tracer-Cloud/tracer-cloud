985567e535127f902856ad14d3e18494dd365085
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-02-26 12:46:50 +0000

    add rndm print

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use tracer::cli::process_cli;
 
 pub fn main() {
     if let Err(err) = process_cli() {
-        eprintln!("Error processing Cli: {err}");
+        eprintln!("Error processing Cli print: {err}");
     }
 }
